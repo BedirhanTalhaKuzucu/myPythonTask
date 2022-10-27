a13b47a08431308c6b3ab495b284be3683e129b6
btkkzc <btkkzc@gmail.com>
2022-10-27 15:42:58 +0200

    information was stored in the data base .

--- myTasks/models.py
+++ myTasks/models.py
@@ -1,3 +1,11 @@
+from inspect import iscode
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    idNummer = models.IntegerField(blank=True, null=True)
+    iscode = models.CharField(max_length=5)
+
+
+    def __str__(self):
+        return self.name
