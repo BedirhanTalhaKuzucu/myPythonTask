2eebab6772d581a08c4c59fc52ed4b566c2ed863
btkkzc <btkkzc@gmail.com>
2022-10-27 14:09:44 +0200

    part about mission 2 has been fixed

--- myTasks/views.py
+++ myTasks/views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import render
 import json
+from django.shortcuts import render
 import logging
 logger = logging.getLogger("django")
-def home(request):
-    
-    context = {
-        'data': [
+
+
+countryData = {
+    'infoList': [
             {"name" : "Afghanistan",  "ID":93, "ISOCode" : "AF" },
             {"name" : "Germany",  "ID":49, "ISOCode" : "DE" },
             {"name" : "Ghana",  "ID":233, "ISOCode" : "GH" },
             {"name" : "Greenland",  "ID":299, "ISOCode" : "GL" },
             {"name" : "France",  "ID":33, "ISOCode" : "FR" },
         ]
+}
+
+def home(request):
+    # json_data = open('data.json')
+    # data = json.load(json_data)
+    
+    context = {
+        'data': countryData
     }
-    logger.info(context) 
+    logger.info(countryData) 
     return render(request, "myTasks/home.html", context)
