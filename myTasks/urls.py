b126ce63d412d3d6e347ca89fe7b7dbf879ddfec
btkkzc <btkkzc@gmail.com>
2022-10-27 16:41:33 +0200

    Task7 completed. Data ispulled from api and listed

--- myTasks/urls.py
+++ myTasks/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import home, sortedId, sortedISO
+from .views import home, sortedId, sortedISO, renderDatabase, fetchData
 
 
 urlpatterns = [
  path('', home, name="home"),
  path('sortID', sortedId, name="sortedId"),
  path('sortISO', sortedISO, name="sortedISO"),
+ path('dataBase', renderDatabase, name="renderDatabase"),
+ path('api', fetchData, name="fetchData"),
 
 ]
--- myTasks/views.py
+++ myTasks/views.py
@@ -1,5 +1,7 @@
 import json
 from django.shortcuts import render
+from .models import Country
+import requests
 import logging
 logger = logging.getLogger("django")
 
@@ -15,10 +17,12 @@ countryData = {
 }
 
 def home(request):
-    logger.info(countryData) 
+    # logger.info(countryData) 
     sortedId = sorted(countryData['infoList'], key=lambda x: x['ID'])
     sortedISO = sorted(countryData['infoList'], key=lambda x: x['ISOCode'])
-    
+
+    countries = Country.objects.all()
+    logger.info(countries) 
     
     context = {
         'data': countryData,
@@ -42,3 +46,39 @@ def sortedISO(request):
         "sortedISO" :sortedISO,
     }
     return render(request, "myTasks/sortedISO.html", context)
+
+
+def renderDatabase(request):
+    # logger.info(countryData) 
+    # sortedId = sorted(countryData['infoList'], key=lambda x: x['ID'])
+    # sortedISO = sorted(countryData['infoList'], key=lambda x: x['ISOCode'])
+
+    countries = Country.objects.all()
+    logger.info(countries) 
+    
+    context = {
+        'countries': countries, 
+    }
+    return render(request, "myTasks/renderedDataBase.html", context)
+
+def fetchData(request):
+    url = "https://restcountries.com/v3.1/all"
+    dataList = []
+
+    response = requests.request("GET", url)
+    if response.ok:
+        content = response.json()
+        for i in range(5):
+            data = {
+                "name":content[i]["name"]["official"],
+                "ISO": content[i]["cca2"],
+                "id" : content[i]["idd"]["suffixes"][0],
+            }
+            dataList.append(data)
+    
+    print(dataList)
+
+    context = {
+        "dataList" :dataList,
+    }
+    return render(request, "myTasks/fetchData.html", context)
