f9e855043da2d9e66c3fe41a3fd89084e5e8a355
btkkzc <btkkzc@gmail.com>
2022-10-27 13:55:17 +0200

    Task2completed. list printed to web pageandconsole

--- myTasks/views.py
+++ myTasks/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
-
+import json
+import logging
+logger = logging.getLogger("django")
 def home(request):
- return render(request, "myTasks/home.html")
+    
+    context = {
+        'data': [
+            {"name" : "Afghanistan",  "ID":93, "ISOCode" : "AF" },
+            {"name" : "Germany",  "ID":49, "ISOCode" : "DE" },
+            {"name" : "Ghana",  "ID":233, "ISOCode" : "GH" },
+            {"name" : "Greenland",  "ID":299, "ISOCode" : "GL" },
+            {"name" : "France",  "ID":33, "ISOCode" : "FR" },
+        ]
+    }
+    logger.info(context) 
+    return render(request, "myTasks/home.html", context)
